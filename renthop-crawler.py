f67afeff1df2a744792cbdb1460da25c671b8cef
Michael McCarthy <michemcc@outlook.com>
2019-05-27 20:15:56 -0400

    Add number of bed and bath to crawler file

--- renthop-crawler.py
+++ renthop-crawler.py
@@ -6,6 +6,9 @@ import re
 # Initialize columns
 B = []
 C = []
+D = []
+D_bed = []
+D_bath = []
 
 # Define crawl function and crawl through url for max number of pages
 def crawl(url, maxpage):
@@ -32,10 +35,25 @@ def crawl(url, maxpage):
                 for item in price:
                     item = item.strip()
                     C.append(item)
+        # Find both the bed and bath values (same span style) and append to D
+        for row in main_div.findAll("td", {"class": "font-size-11"}):
+            cells = row.findAll("span", {"style": "font-weight: bold; color: #444444;"})
+            for each in cells:
+                bed_bath = each.contents
+                for item in bed_bath:
+                    item = item.strip()
+                    D.append(item)
+
+        # Split D into D_bed and D_bath based on item indexing
+        D_bed = [item for i,item in enumerate(D) if i%2==0]
+        D_bath = [item for i,item in enumerate(D) if i%2==1]
+
         # Increase the count and print data to csv
         count += 1
         df = pd.DataFrame(B, columns=["Neighborhood"])
         df["Price"] = C
+        df["Number of Beds"] = D_bed
+        df["Number of Baths"] = D_bath
         df.to_csv("data.csv")
 
-crawl("https://www.renthop.com/boston-ma/apartments-for-rent?page=",100)
+crawl("https://www.renthop.com/boston-ma/apartments-for-rent?page=",150)
